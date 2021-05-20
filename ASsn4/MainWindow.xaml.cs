f7b7fda69af49ec0cb0d566e9dd81a0d0909050b
Patrice Briggs <PBriggs@perkinscoie.com>
2021-05-20 09:13:55 -0700

    Initial commit HW4

---
+++ ASsn4/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ASsn4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtZipCodeToCheck_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // T1R2X4
+            string checkZip = @"^(\d{5}(-\d{4})?|[A-Z]\d[A-Z] ?\d[A-Z]\d)$";  // from https://www.regextester.com/103297 
+            //"^[0-9]{5}(?:-[0-9]{4})?$";  <<== works for US zip codes
+
+            if (Regex.IsMatch(txtZipCodeToCheck.Text,checkZip))
+            {
+                btnGoodZip.IsEnabled = true;
+            }
+            else
+            {
+                btnGoodZip.IsEnabled = false;
+            }
+        }
+    }
+}
